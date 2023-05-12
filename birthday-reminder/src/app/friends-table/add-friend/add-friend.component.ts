29356790eed6b860a6a48e715543d19864b5fe22
Nicoleta Hermeneanu <hermeneanunicoleta14@yahoo.com>
2023-05-12 14:20:24 +0300

    Add friend form html

--- birthday-reminder/src/app/friends-table/add-friend/add-friend.component.ts
+++ birthday-reminder/src/app/friends-table/add-friend/add-friend.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-friend',
   templateUrl: './add-friend.component.html',
-  styleUrls: ['./add-friend.component.scss']
+  styleUrls: ['./add-friend.component.scss'],
 })
 export class AddFriendComponent implements OnInit {
+  addFriendForm: UntypedFormGroup;
 
-  constructor() { }
+  constructor(private fb: UntypedFormBuilder, private router: Router) {}
 
   ngOnInit(): void {
+    this.addFriendForm = this.fb.group({
+      first_name: [null, [Validators.required]],
+      last_name: [null, [Validators.required]],
+      phone_number: [null, [Validators.required]],
+      city: [null, [Validators.required]],
+      birthday_date: [null, [Validators.required]],
+    });
   }
 
+  add() {}
+
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.addFriendForm.reset();
+    for (const key in this.addFriendForm.controls) {
+      if (this.addFriendForm.controls.hasOwnProperty(key)) {
+        this.addFriendForm.controls[key].markAsPristine();
+        this.addFriendForm.controls[key].updateValueAndValidity();
+      }
+    }
+  }
 }
--- birthday-reminder/src/app/friends-table/friends-table-routing.module.ts
+++ birthday-reminder/src/app/friends-table/friends-table-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FriendsComponent } from './friends/friends.component';
+import { AddFriendComponent } from './add-friend/add-friend.component';
 
 const routes: Routes = [
   {
     path: 'friends',
     component: FriendsComponent,
   },
+  {
+    path: 'add-friend',
+    component: AddFriendComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FriendsTableRoutingModule { }
+export class FriendsTableRoutingModule {}
