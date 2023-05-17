3e80e06936a843eaafa1d7dd24963021eb8d5989
Nicoleta Hermeneanu <hermeneanunicoleta14@yahoo.com>
2023-05-17 16:22:50 +0300

    Friend details page style

--- birthday-reminder/src/app/helpers/custom-validators.ts
+++ birthday-reminder/src/app/helpers/custom-validators.ts
@@ -1,37 +1,42 @@
-import { AbstractControl, ValidationErrors, FormControl, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  ValidationErrors,
+  FormControl,
+  ValidatorFn,
+} from '@angular/forms';
 
 export class CustomValidators {
-    static passwordStrength(): ValidatorFn {
-        return (control: AbstractControl): { [key: string]: any } | null => {
-          const value: string = control.value;
-          const hasLowerCase = /[a-z]/.test(value);
-          const hasUpperCase = /[A-Z]/.test(value);
-          const hasNumber = /\d/.test(value);
-    
-          if (!hasLowerCase || !hasUpperCase || !hasNumber) {
-            return { passwordStrength: true };
-          }
-    
-          return null;
-        };
-    }
+  static passwordStrength(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const value: string = control.value;
+      const hasLowerCase = /[a-z]/.test(value);
+      const hasUpperCase = /[A-Z]/.test(value);
+      const hasNumber = /\d/.test(value);
 
-    static dateValidator: ValidatorFn = (
-      control: AbstractControl
-    ): { [key: string]: any } | null => {
-      let date = control.value;
-  
-      if (date === '' || date == null) {
-        return null;
+      if (!hasLowerCase || !hasUpperCase || !hasNumber) {
+        return { passwordStrength: true };
       }
-  
-      const selectedDate = new Date(control.value);
-      const currentDate = new Date();
-  
-      if (selectedDate > currentDate) {
-        return { dateInFuture: true };
-      }
-  
+
       return null;
     };
   }
+
+  static dateValidator: ValidatorFn = (
+    control: AbstractControl
+  ): { [key: string]: any } | null => {
+    let date = control.value;
+
+    if (date === '' || date == null) {
+      return null;
+    }
+
+    const selectedDate = new Date(control.value);
+    const currentDate = new Date();
+
+    if (selectedDate > currentDate) {
+      return { futureDate: true };
+    }
+
+    return null;
+  };
+}
